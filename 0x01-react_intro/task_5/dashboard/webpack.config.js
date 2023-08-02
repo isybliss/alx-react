6301af7c1f08245a685ad0183ea83481f3562cce
isybliss <isyblissumunna@gmail.com>
2023-08-02 22:47:05 +0100

    create project using webpack

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,11 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
   },
   mode: "development",
   module: {
@@ -27,11 +25,6 @@ module.exports = {
             },
           },
         ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
       },
     ],
   },
